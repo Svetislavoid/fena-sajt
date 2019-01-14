ec58967e31cd28d5c9da4773b5c5043765644d40
Svetislav Grujić <svetislav.grujic@gmail.com>
2019-01-14 02:52:27 +0100

    Updates

---
+++ precache-manifest.e3e4e6f742d0916fa48dd0f3c7e66303.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "12456e03947d2f932f4e",
+    "url": "/Svetislavoid/fena-sajt/static/css/main.1fca6560.chunk.css"
+  },
+  {
+    "revision": "12456e03947d2f932f4e",
+    "url": "/Svetislavoid/fena-sajt/static/js/main.12456e03.chunk.js"
+  },
+  {
+    "revision": "9dc3d7a788ff14f347c3",
+    "url": "/Svetislavoid/fena-sajt/static/js/1.9dc3d7a7.chunk.js"
+  },
+  {
+    "revision": "5bf8beb92f2c44c3d50c",
+    "url": "/Svetislavoid/fena-sajt/static/js/runtime~main.5bf8beb9.js"
+  },
+  {
+    "revision": "8906d56b825ce6f99356cee45200773a",
+    "url": "/Svetislavoid/fena-sajt/static/media/fena-bg.8906d56b.svg"
+  },
+  {
+    "revision": "02b4bc1b42bdd25956d7a2eac1203cf3",
+    "url": "/Svetislavoid/fena-sajt/static/media/fena-logo-senka.02b4bc1b.svg"
+  },
+  {
+    "revision": "d27da8bc8c29fd016d1f036974bf7914",
+    "url": "/Svetislavoid/fena-sajt/static/media/dtco3.d27da8bc.svg"
+  },
+  {
+    "revision": "6c5b3ef5d301b456c23f1bfcb5ac2c73",
+    "url": "/Svetislavoid/fena-sajt/static/media/1324mtco.6c5b3ef5.svg"
+  },
+  {
+    "revision": "d04ac25673098d114e423aa632083dbf",
+    "url": "/Svetislavoid/fena-sajt/static/media/autoput.d04ac256.jpg"
+  },
+  {
+    "revision": "c82b2e3c7a075ab7c8ab3983a14a2f87",
+    "url": "/Svetislavoid/fena-sajt/static/media/fena-logo.c82b2e3c.svg"
+  },
+  {
+    "revision": "b759bd1684ea66cdb342e86da51a8451",
+    "url": "/Svetislavoid/fena-sajt/index.html"
+  }
+];
