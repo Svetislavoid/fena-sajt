e53dc6893e1cb33ce2e7fe1f6ed590e418f4f78f
Svetislav Grujić <svetislav.grujic@gmail.com>
2019-01-14 03:09:12 +0100

    Podešavanje za gh-pages.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from './Logo.module.css';
 import LogoSenka from '../../assets/fena-logo-senka.svg';
-import FenaBG from '../../assets/fena-bg.svg';
 
 const logo = (props) => (
 	<div className={classes.Logo}>
